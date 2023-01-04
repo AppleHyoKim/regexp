8901bd3fe28bebf58b39adc95ee4c4bfc3c0ec2a
Kimhyojin <applekim@lfcorp.com>
2023-01-04 13:25:57 +0900

    .

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-let str = `hello world
-010-1234-5678
-the
+const str = `hello world
+010-1234-5678.
+thedfjlsdjf@gmail.com
 https://www.omdbapi.com/?apikey=349827459=frozen
-The quick brown fox jumps over the lazy dog.
-abbcccddd`;
-
-// const regexp = RegExp('the', 'gi');
-const regexp = /fox/gi;
-// console.log(srt.match(regexp));
-// console.log(regexp.test(str));
-str = str.replace(regexp, 'ddddddddddd');
-console.log(str)
+The quick brown fox jumps over the lazy ddog.
+abbcccdddd
+http://www.omdbapi.com/?apikey=349827459=frozen
+동해물과_백두산이_마르고 닳도록
+`;
+console.log(
+    str.match(/(?<=@).{1,}/g)
+);
